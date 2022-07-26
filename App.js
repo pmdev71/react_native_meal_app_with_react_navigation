6d2dbb092640ff93cdb3df944e8e3c11d14944d2
Palash Mandal <pmpalashmandal@hmail.com>
2022-07-27 01:40:55 +0600

    oneSignal push notification added

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, View, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import OneSignal from 'react-native-onesignal';
 
 import 'react-native-gesture-handler';
 
@@ -12,6 +14,17 @@ import MealDetailScreen from './screens/MealDetailScreen';
 const Stack = createStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    //OneSignal Init Code
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setAppId('d516205c-d8a6-44ad-b9f2-8e307473acca');
+    //END OneSignal Init Code
+
+    //Method for handling notifications opened
+    OneSignal.setNotificationOpenedHandler(notification => {
+      console.log('OneSignal: notification opened:', notification);
+    });
+  });
   return (
     <>
       <StatusBar style="light" />
